78c4827f8784163871c09c0f67b13469797fc364
Miklos Maroti <mmaroti@gmail.com>
2023-03-02 22:20:01 +0100

    playing around with build options

---
+++ src/pybind11.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023 Miklos Maroti.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include <CGAL/version.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_diffmesh, m)
+{
+    m.doc() = "Differentiable meshing C++ library";
+
+    m.attr("CGAL_VERSION_STR") = CGAL_VERSION_STR;
+}
